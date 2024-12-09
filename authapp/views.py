ef5d7472ad658eb147f839da121eb3289c143633
Nitay Das <nitaydas910@gmail.com>
2024-12-09 16:05:54 +0600

    initial commit

---
+++ authapp/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({"message": "Hello, React!"})
+
+
+def home(request):
+    return render(request, 'Home/home.html')
