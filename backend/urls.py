a7a73f4fe47a7432333c7d7bae45f6aac81f135b
Nitay Das <nitaydas910@gmail.com>
2024-12-11 17:25:15 +0600

    Added Room

--- backend/urls.py
+++ backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mainapp/', include('mainapp.urls')),
+    path('', include('mainapp.urls')),
     path('', include('authapp.urls')),
 ] 
 
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     
+    path('room/', RoomListCreateAPIView.as_view(), name='room-list'), 
 ]
