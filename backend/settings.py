3d27bf5008583df393da5a540d216b951f2797ad
Nitay Das <nitaydas910@gmail.com>
2025-01-04 16:03:02 +0600

    Changes from remote

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'django-insecure-s&(&-zrw(l#88)w6m90m_-&b5+0$lbt7yd&*@16^nigcq8wz0&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'authapp.User'
 ALLOWED_HOSTS = ['chjashore.online', 'www.chjashore.online', '86.48.3.219', '127.0.0.1']
@@ -125,23 +125,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'circuithouse',
-        'USER': 'ndcjashore1',
-        'PASSWORD': '123456NdcJ',
-        'HOST': '86.48.3.219',  
-        'PORT': '5432', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'circuithouse',
+#         'USER': 'ndcjashore1',
+#         'PASSWORD': '123456NdcJ',
+#         'HOST': '86.48.3.219',  
+#         'PORT': '5432', 
+#     }
+# }
 
 
 # Password validation
